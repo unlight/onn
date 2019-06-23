6869f4a2a8e4bb9c3a026a89e18d1f701490a47c
roman.vasilev <s91630277@gmail.com>
2019-06-23 20:18:10 +0400

    chore: Added 4th argument

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export function on(target: EventTarget, type: string, listener: EventListenerOrEventListenerObject) {
-    target.addEventListener(type, listener, false);
+export function on(target: EventTarget, type: string, listener: EventListenerOrEventListenerObject, options: boolean | AddEventListenerOptions = false) {
+    target.addEventListener(type, listener, options);
     return function off() {
-        target.removeEventListener(type, listener, false);
+        target.removeEventListener(type, listener, options);
     };
 }
