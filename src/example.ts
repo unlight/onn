87e8f145c58aaa301a54bb643ffd670dc2dd7653
roman.vasilev <s91630277@gmail.com>
2019-06-23 21:32:04 +0400

    docs: Updated README

--- src/example.ts
+++ src/example.ts
@@ -1,13 +1,7 @@
-# onn
-Event emitter using DOM's addEventListener/dispatchEvent
-
-## Usage
-```js
-const { on } = require('onn');
+const { on } = require('./index');
 
 const off = on(document, 'login', (event) => {
     console.log(`Hello, ${event.detail.name}!`); // => Hello, Bob!
 });
 document.dispatchEvent(new CustomEvent('login', { detail: { name: 'Bob' } }));
 off(); // Unsubscribe
-```
